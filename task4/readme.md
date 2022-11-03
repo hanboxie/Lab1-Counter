e2456282784e01508bf44f461dec46607b6af004
Hanbo Xie <xhanbo@LAPTOP-BC79T2RO.localdomain>
2022-11-03 10:11:42 +0000

    updates

--- task4/readme.md
+++ task4/readme.md
@@ -6,6 +6,12 @@
 - input signals at the correct time
 - display output signals
 
-2. Modify doit.sh
+2. Modify Top.sv
+For specific changes, refer to `top_tb.cpp` file. Main changes are just renaming things such as Vcounter to Vtop.
 
-### Step 2
+3. Modify doit.sh
+![code](newdoit.png)
+
+Plain name changes of files and inclusion of the extra .sv files
+
+### Works according to expectation: increments by 1 and shows BCD numbers
--- task4/top_tb.cpp
+++ task4/top_tb.cpp
@@ -1,53 +1,58 @@
+//Header files (not name Vcounter.h for module counter)
 #include "Vtop.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 #include "vbuddy.cpp"
 
-int main (int argc, char **argv, char **env) {
+int main(int argc, char **argv, char **env) {
     int i;
     int clk;
 
     Verilated::commandArgs(argc, argv);
-    // init top verilog instance
+    //instatiate the counter module as Vcounter
+    //this is the DUT (name of all generated files)
     Vtop* top = new Vtop;
-    // init trace dump
+    //turn on signal tracing and tell Verilator to
+    //dump the waveform data to counter.vcd
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
-    top->trace (tfp,99);
+    top->trace (tfp, 99);
     tfp->open ("top.vcd");
 
-    // init Vbuddy
-    if (vbdOpen()!=1) return (-1);
-    vbdHeader("Lab 1: Counter");
-
-    // initialize simulation inputs
+    //init Vbuddy
+    if(vbdOpen()!=1) return(-1);
+    vbdHeader("Lab1 : top");
+    
+    //initialize simulation inputs
     top->clk = 1;
     top->rst = 0;
-    top->en = 0;
-    top->bcd = 1;
+    top->en = 1;
 
-    // run simulation for many clock cycles
-    for (int i=0; i<300; i++) {
 
-        // dump variables into VCD file and toggle clock
+    //run simulation for many clock cycles
+    for (i=0; i<300; i++) {
+
+        //dump variables into VCD file and toggle clock
         for (clk=0; clk<2; clk++) {
             tfp->dump (2*i+clk);
             top->clk = !top->clk;
-            top->eval ();
+            top->eval();
         }
-
+        
         // Send count value to Vbuddy
-        vbdHex(4, (int(top->bcd) >> 16) & 0xF);
-        vbdHex(3, (int(top->bcd) >> 8) & 0xF);
-        vbdHex(2, (int(top->bcd) >> 4) & 0xF);
+        vbdHex(4, (int(top->bcd))>> 12 & 0xF);
+        vbdHex(3, (int(top->bcd))>> 8 & 0xF);
+        vbdHex(2, (int(top->bcd))>> 4 & 0xF);
         vbdHex(1, (int(top->bcd)) & 0xF);
         vbdCycle(i+1);
         // end of Vbuddy output section
-
-        if(Verilated::gotFinish()) exit(0);
+        
+        //change input stimuli
+        
+        if (Verilated::gotFinish()) exit(0);
     }
-
-    vbdClose(); //+++
+    
+    vbdClose();
     tfp->close();
     exit(0);
 }
